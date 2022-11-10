11d6c07e278b742940b377022a79f7a5cb289b01
Anthony Carreon <anthonycarreon@zoho.com>
2022-11-10 11:12:07 -0800

    Initial commit

---
+++ src/ScoreTable.java
@@ -0,0 +1,7 @@
+// Name: Anthony Carreon
+// USC NetID: Carreona
+// CS 455 PA4
+// Fall 2022
+
+public class ScoreTable {
+}
---
+++ src/WordFinder.java
@@ -0,0 +1,10 @@
+// Name: Anthony Carreon
+// USC NetID: Carreona
+// CS 455 PA4
+// Fall 2022
+
+public class WordFinder {
+   public static void main(String[] args) {
+      System.out.println(Rack.);;
+   }
+}
